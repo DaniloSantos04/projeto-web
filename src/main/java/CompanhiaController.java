26fe51b3a50faf136d134371299b3899ba43bee2
Danilo Santos <danilosantos@dcc.ufba.br>
2017-08-22 22:11:55 -0300

    ajustes no projeto

--- src/main/java/CompanhiaController.java
+++ src/main/java/CompanhiaController.java
@@ -1,9 +1,9 @@
 import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 
 import business.CompanhiaBusiness;
 
@@ -11,9 +11,11 @@ import business.CompanhiaBusiness;
 @ViewScoped
 public class CompanhiaController implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+
 	private Companhia companhia = new Companhia();
 	
-	@EJB
+	@Inject
 	CompanhiaBusiness companhiaBusiness;
 	
 	@PostConstruct
